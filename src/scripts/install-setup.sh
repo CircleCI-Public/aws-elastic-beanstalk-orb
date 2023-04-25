c14d6908a050cb81001600b248cf3d415d8c9d41
Moo Olaniyan <moo@circleci.com>
2023-04-25 13:57:20 -0600

    testing: removed AWS env var check, not needed for test

--- src/scripts/install-setup.sh
+++ src/scripts/install-setup.sh
@@ -75,6 +75,6 @@ CheckAWSEnvVars() {
 # View src/tests for more information.
 TEST_ENV="bats-core"
 if [ "${0#*"$TEST_ENV"}" == "$0" ]; then
-    CheckAWSEnvVars
+    #CheckAWSEnvVars
     InstallEBCLI
 fi
