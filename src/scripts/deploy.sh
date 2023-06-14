d08a02cac0a5b9a361da4f200d2d1660d2064119
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-06-14 11:59:23 -0700

    chore: update src/scripts/deploy.sh
    Co-authored-by: Eric Ribeiro <ericribeiro@outlook.com.br>

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-ORB_STR_APPLICATION_NAME=$(circleci env subst "${ORB_STR_APPLICATION_NAME}")
-ORB_STR_PLATFORM_VERSION=$(circleci env subst "${ORB_STR_PLATFORM_VERSION}")
-ORB_STR_ENVIRONMENT_NAME=$(circleci env subst "${ORB_STR_ENVIRONMENT_NAME}")
-ORB_STR_LABEL=$(circleci env subst "${ORB_STR_LABEL}")
-ORB_STR_DESCRIPTION=$(circleci env subst "${ORB_STR_DESCRIPTION}")
-ORB_STR_PROFILE_NAME=$(circleci env subst "${ORB_STR_PROFILE_NAME}")
-ORB_STR_REGION=$(circleci env subst "${ORB_STR_REGION}")
+ORB_STR_APPLICATION_NAME="$(circleci env subst "${ORB_STR_APPLICATION_NAME}")"
+ORB_STR_PLATFORM_VERSION="$(circleci env subst "${ORB_STR_PLATFORM_VERSION}")"
+ORB_STR_ENVIRONMENT_NAME="$(circleci env subst "${ORB_STR_ENVIRONMENT_NAME}")"
+ORB_STR_LABEL="$(circleci env subst "${ORB_STR_LABEL}")"
+ORB_STR_DESCRIPTION="$(circleci env subst "${ORB_STR_DESCRIPTION}")"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
+ORB_STR_REGION="$(circleci env subst "${ORB_STR_REGION}")"
 
 if [ -n "${ORB_STR_LABEL}" ]; then
     set -- "$@" -l "${ORB_STR_LABEL}"
