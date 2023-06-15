ad5422f2b9a6c31553ae8a9a34d972a80ce57768
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-06-15 10:31:09 -0700

    feat: v3.0 (#30)
    * refactor: update variable names
    
    * chore: rename parameter using snake case
    
    * feat: add region parameter
    
    * chore: update src/scripts/deploy.sh
    
    Co-authored-by: Eric Ribeiro <ericribeiro@outlook.com.br>
    
    * chore: update src/scripts/install-setup.sh
    
    Co-authored-by: Eric Ribeiro <ericribeiro@outlook.com.br>
    
    ---------
    
    Co-authored-by: Eric Ribeiro <ericribeiro@outlook.com.br>

--- src/scripts/install-setup.sh
+++ src/scripts/install-setup.sh
@@ -27,7 +27,7 @@ SetupPipx() {
 
 
 InstallEBCLI() {
-    ORB_EVAL_EB_CLI_VERSION=$(circleci env subst "${ORB_EVAL_EB_CLI_VERSION}")
+    ORB_STR_EB_CLI_VERSION="$(circleci env subst "${ORB_STR_EB_CLI_VERSION}")"
     if uname -a | grep Darwin > /dev/null 2>&1; then
         cd /tmp || { echo "Not able to access /tmp"; return; }
         git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
@@ -47,8 +47,8 @@ InstallEBCLI() {
         fi
     fi
     # If the input version environment variable is set, install that version. Else install latest.
-    if [ -n "$ORB_EVAL_EB_CLI_VERSION" ]; then
-        pipx install awsebcli=="${ORB_EVAL_EB_CLI_VERSION}"
+    if [ -n "$ORB_STR_EB_CLI_VERSION" ]; then
+        pipx install awsebcli=="${ORB_STR_EB_CLI_VERSION}"
         echo "Complete"
     else
         pipx install awsebcli
